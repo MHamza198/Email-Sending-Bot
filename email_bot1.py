f46656f1dddbcc7a445acf59fd6ddb6c047e0594
Muhammad Hamza <80377241+MHamza198@users.noreply.github.com>
2023-08-21 20:43:38 +0500

    Version 2.0
    I make the User Interface for the Bot

--- email_bot1.py
+++ email_bot1.py
@@ -1,26 +1,21 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import pandas as pd
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
 
 # Gmail account details
-sender_email = 'muhammadhamza198198@gmail.com'  # Replace with your Gmail email
-sender_password = 'wjwlgfcucorbkfjg'      # Replace with your Gmail password
-
-# Load recipient email addresses from Excel file
-def load_recipients_from_excel(filename):
-    try:
-        df = pd.read_excel(filename)
-        return df['Email'].tolist()
-    except Exception as e:
-        print(f"Error loading recipients from Excel: {e}")
-        return []
+sender_email = 'michelmarsh88@gmail.com'  # Replace with your Gmail email
+sender_password = 'Put your own'      # Replace with your Gmail password
 
 # Email content
-subject = 'Your Subject'
-message = 'Your Message'
+subject = ''
+message = ''
 
-def send_email(subject, message, recipient):
+def send_email_to_recipient(subject, message, recipient):
     try:
         msg = MIMEMultipart()
         msg['From'] = sender_email
@@ -37,10 +32,29 @@ def send_email(subject, message, recipient):
     except Exception as e:
         print(f"Error sending email to {recipient}: {e}")
 
-# Load recipient emails from Excel file
-recipient_file = 'recipient_list.xlsx'  # Replace with your Excel file name
-recipient_list = load_recipients_from_excel(recipient_file)
+def load_recipients_from_excel(filename):
+    try:
+        df = pd.read_excel(filename)
+        return df['Email'].tolist()
+    except Exception as e:
+        print(f"Error loading recipients from Excel: {e}")
+        return []
 
-# Sending emails to recipients
-for recipient in recipient_list:
-    send_email(subject, message, recipient)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        global subject, message
+        subject = request.form['subject']
+        message = request.form['message']
+        recipient_file = request.files['file']
+        if recipient_file:
+            filename = os.path.join('uploads', recipient_file.filename)
+            recipient_file.save(filename)
+            recipient_list = load_recipients_from_excel(filename)
+            for recipient in recipient_list:
+                send_email_to_recipient(subject, message, recipient)
+            os.remove(filename)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
